22fa8bb4f32fb6b9404888e3ae1da00b7ed55112
KollihTheGuruh <mmike8449@gmail.com>
2023-12-07 18:56:55 -0500

    rm

--- Project/laptopbazaar/mainapp/forms/registration_form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
