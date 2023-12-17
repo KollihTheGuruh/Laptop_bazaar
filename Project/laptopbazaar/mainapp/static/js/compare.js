9cd6f4f6459ae102e15832cd4f3c3a10bfca6781
KollihTheGuruh <mmike8449@gmail.com>
2023-12-16 21:08:50 -0500

    compare.html changes

--- Project/laptopbazaar/mainapp/static/js/compare.js
+++ Project/laptopbazaar/mainapp/static/js/compare.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
             comparisonList.push(laptopId);
             // Optionally, you can store the comparisonList in local storage for persistence
             localStorage.setItem('comparisonList', JSON.stringify(comparisonList));
+            updateComparisonView();
         }
     }
 
@@ -19,16 +20,32 @@ document.addEventListener('DOMContentLoaded', function() {
             comparisonList.splice(index, 1);
             // Update local storage if you're using it
             localStorage.setItem('comparisonList', JSON.stringify(comparisonList));
+            updateComparisonView();
         }
     }
 
     // Function to update the comparison view
     function updateComparisonView() {
+        console.log('Updating comparison view...');
         // Fetch the details of laptops to be compared using comparisonList
         // You can make an API call to get laptop details based on the IDs in comparisonList
 
         // Update the DOM to reflect the comparison list
         // This might involve dynamically creating a table or list in your HTML
+        const comparisonContainer = document.querySelector('.compare-container');
+        if (comparisonContainer) {
+            if (comparisonList.length > 0) {
+                comparisonContainer.innerHTML = '<p>Comparing Laptops:</p>';
+                comparisonList.forEach(laptopId => {
+                    // Fetch laptop details based on ID and update the DOM
+                    comparisonContainer.innerHTML += `<p>Laptop ID: ${laptopId}</p>`;
+                });
+                // Optionally, you can add a button or link to navigate to the compare page
+                comparisonContainer.innerHTML += `<a href="{% url 'compare_view' %}?laptop_ids=${comparisonList.join(',')}" class="btn compare-btn">Compare Now</a>`;
+            } else {
+                comparisonContainer.innerHTML = '<p>Please select laptops to compare.</p>';
+            }
+        }
     }
 
     // Event listener for 'Compare' buttons
@@ -37,7 +54,6 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function(event) {
             const laptopId = this.dataset.laptopId;
             addToCompare(laptopId);
-            updateComparisonView();
         });
     });
 
@@ -47,12 +63,12 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function(event) {
             const laptopId = this.dataset.laptopId;
             removeFromCompare(laptopId);
-            updateComparisonView();
         });
     });
 
     // Function to clear the comparison list
     function clearComparison() {
+        console.log('Clearing comparison...');
         // Clear the comparisonList array
         comparisonList = [];
         // Optionally, remove the comparisonList from local storage
